12c49726ad5faf04d6fcff8adc62026d05ec894b
indieleet <indieleet@gmail.com>
2024-08-07 17:06:38 -0400

    meh

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::io::{stdout, Result};
+use std::{
+    alloc::Layout,
+    io::{stdout, Result},
+};
 
 use ratatui::{
     backend::CrosstermBackend,
@@ -7,16 +10,19 @@ use ratatui::{
         terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
         ExecutableCommand,
     },
-    style::Stylize,
     prelude::*,
+    style::Stylize,
     widgets::*,
     Terminal,
 };
 
 #[derive(Debug, Default)]
-pub struct App {
+pub struct App<'a> {
     counter: u16,
     cursor: u8,
+    items: Vec<Constraint>,
+    rows: Vec<Row<'a>>,
+    constrains: Vec<Constraint>,
 }
 
 fn main() -> Result<()> {
@@ -24,40 +30,45 @@ fn main() -> Result<()> {
     enable_raw_mode()?;
     let mut terminal = Terminal::new(CrosstermBackend::new(stdout()))?;
     terminal.clear()?;
-    let mut app = App { counter: 1, cursor: 0 };
-    loop { 
-        terminal.draw(|frame| {
-            let area = frame.size();
-            for i in 0..app.counter as usize { 
-                let cur_width = area.width / app.counter;
-                frame.render_widget(
-                    Block::new()
-                    .title(format!("Hello Ratatui!, {} (press 'q' to quit)", app.counter))
-                    .borders(Borders::LEFT)
-                    .white()
-                    .on_blue(),
-                    ratatui::layout::Rect{
-                        x: area.x + cur_width * i as u16, 
-                        y: area.y,
-                        width: cur_width,
-                        height: area.height },
-                );}
-        })?;   // TODO main loop
-        if event::poll(std::time::Duration::from_millis(16))? {
-            if let event::Event::Key(key) = event::read()? {
-                if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('q') {
-                    break;
-                }
+    let mut app = App {
+        counter: 1,
+        cursor: 0,
+        items: vec![Constraint::Percentage(30), Constraint::Fill(1)],
+        rows: vec![Row::new(vec!["7/9 1/2 1"])],
+        constrains: vec![Constraint::Percentage(30)],
+    };
+    loop {
+        terminal.draw(|f| {
+            let size_x = ratatui::layout::Layout::default()
+                .direction(Direction::Horizontal)
+                .constraints([Constraint::Percentage(50), Constraint::Percentage(50)])
+                .split(f.size());
+            let rows = [Row::new(vec!["7/9 1/2 1"])];
+            let constr = vec![Constraint::Percentage(15)];
+            f.render_widget(
+                Table::new(app.rows.clone(), app.constrains.clone()).block(Block::bordered()),
+                size_x[0],
+            );
+            f.render_widget(Paragraph::new("Hello").block(Block::bordered()), size_x[1]);
+        })?;
+        // TODO main loop
+        if let event::Event::Key(key) = event::read()? {
+            if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('q') {
+                break;
             }
-            if let event::Event::Key(key) = event::read()? {
-                if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('+') {
-                    app.counter = app.counter.saturating_add(1);
-                }
+        }
+        if let event::Event::Key(key) = event::read()? {
+            if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('+') {
+                app.counter = app.counter.saturating_add(1);
+                app.rows.push(Row::new(vec!["7/9 1/2 1"]));
+                app.constrains.push(Constraint::Percentage(30));
             }
-            if let event::Event::Key(key) = event::read()? {
-                if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('-') {
-                    app.counter = app.counter.saturating_sub(1);
-                }
+        }
+        if let event::Event::Key(key) = event::read()? {
+            if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('-') {
+                app.counter = app.counter.saturating_sub(1);
+                app.rows.pop();
+                app.constrains.pop();
             }
         }
     }
