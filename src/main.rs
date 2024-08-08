5e2baf12ca98b2860f2310726bd836196a979629
indieleet <indieleet@gmail.com>
2024-08-08 21:56:05 +0300

    now something works

--- src/main.rs
+++ src/main.rs
@@ -35,40 +35,39 @@ fn main() -> Result<()> {
         cursor: 0,
         items: vec![Constraint::Percentage(30), Constraint::Fill(1)],
         rows: vec![Row::new(vec!["7/9 1/2 1"])],
-        constrains: vec![Constraint::Percentage(30)],
+        constrains: vec![Constraint::Min(1)],
     };
     loop {
         terminal.draw(|f| {
             let size_x = ratatui::layout::Layout::default()
-                .direction(Direction::Horizontal)
+                .direction(Direction::Vertical)
                 .constraints([Constraint::Percentage(50), Constraint::Percentage(50)])
                 .split(f.size());
-            let rows = [Row::new(vec!["7/9 1/2 1"])];
-            let constr = vec![Constraint::Percentage(15)];
             f.render_widget(
-                Table::new(app.rows.clone(), app.constrains.clone()).block(Block::bordered()),
+                Table::new(app.rows.to_owned(), app.constrains.to_owned()).block(Block::bordered()),
                 size_x[0],
             );
             f.render_widget(Paragraph::new("Hello").block(Block::bordered()), size_x[1]);
         })?;
         // TODO main loop
         if let event::Event::Key(key) = event::read()? {
-            if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('q') {
-                break;
-            }
-        }
-        if let event::Event::Key(key) = event::read()? {
-            if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('+') {
-                app.counter = app.counter.saturating_add(1);
-                app.rows.push(Row::new(vec!["7/9 1/2 1"]));
-                app.constrains.push(Constraint::Percentage(30));
-            }
-        }
-        if let event::Event::Key(key) = event::read()? {
-            if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('-') {
-                app.counter = app.counter.saturating_sub(1);
-                app.rows.pop();
-                app.constrains.pop();
+            if key.kind == KeyEventKind::Press {
+                match key.code {
+                    KeyCode::Char('q') => {
+                        break;
+                    }
+                    KeyCode::Char('+') => {
+                        app.counter = app.counter.saturating_add(1);
+                        app.rows.push(Row::new(vec!["7/9 1/2 1"]));
+                        //app.constrains.push(Constraint::Percentage(30));
+                    }
+                    KeyCode::Char('-') => {
+                        app.counter = app.counter.saturating_sub(1);
+                        app.rows.pop();
+                        //app.constrains.pop();
+                    }
+                    _ => (),
+                }
             }
         }
     }
