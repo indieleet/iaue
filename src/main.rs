d48471c6eed2ee9ade85af752e6c6cd2cd08cbbe
indieleet <indieleet@gmail.com>
2024-08-09 04:36:01 -0400

    basic cursor, rows, columns

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 use std::{
     alloc::Layout,
-    io::{stdout, Result},
+    fmt::format,
+    io::{stdout, Cursor, Result},
 };
 
 use ratatui::{
     backend::CrosstermBackend,
     crossterm::{
+        cursor,
         event::{self, KeyCode, KeyEventKind},
         terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
         ExecutableCommand,
@@ -17,12 +19,19 @@ use ratatui::{
 };
 
 #[derive(Debug, Default)]
-pub struct App<'a> {
+pub struct App {
     counter: u16,
     cursor: u8,
     items: Vec<Constraint>,
-    rows: Vec<Row<'a>>,
+    columns: u8,
+    rows: Vec<Vec<String>>,
     constrains: Vec<Constraint>,
+}
+
+#[derive(Debug, Default)]
+struct AppCursor {
+    x: u16,
+    y: u16,
 }
 
 fn main() -> Result<()> {
@@ -30,12 +39,17 @@ fn main() -> Result<()> {
     enable_raw_mode()?;
     let mut terminal = Terminal::new(CrosstermBackend::new(stdout()))?;
     terminal.clear()?;
+    let mut cursor = AppCursor::default();
     let mut app = App {
         counter: 1,
         cursor: 0,
+        columns: 1,
         items: vec![Constraint::Percentage(30), Constraint::Fill(1)],
-        rows: vec![Row::new(vec!["7/9 1/2 1"])],
-        constrains: vec![Constraint::Min(1)],
+        rows: vec![
+            vec!["7/9 1/2 1".into(), "1/2 3/5 1/2".into()],
+            vec!["3/2 1 1".into()],
+        ],
+        constrains: vec![Constraint::Max(10)],
     };
     loop {
         terminal.draw(|f| {
@@ -44,10 +58,26 @@ fn main() -> Result<()> {
                 .constraints([Constraint::Percentage(50), Constraint::Percentage(50)])
                 .split(f.size());
             f.render_widget(
-                Table::new(app.rows.to_owned(), app.constrains.to_owned()).block(Block::bordered()),
+                Table::new(
+                    app.rows.to_owned().into_iter().map(Row::new),
+                    app.constrains.to_owned(),
+                )
+                .block(Block::bordered()),
                 size_x[0],
             );
-            f.render_widget(Paragraph::new("Hello").block(Block::bordered()), size_x[1]);
+            f.render_widget(
+                Paragraph::new(format!("Hello {}", app.counter)).block(Block::bordered()),
+                size_x[1],
+            );
+            f.render_widget(
+                " ".on_red(),
+                layout::Rect {
+                    x: cursor.x,
+                    y: cursor.y,
+                    width: 1,
+                    height: 1,
+                },
+            )
         })?;
         // TODO main loop
         if let event::Event::Key(key) = event::read()? {
@@ -56,15 +86,27 @@ fn main() -> Result<()> {
                     KeyCode::Char('q') => {
                         break;
                     }
+                    KeyCode::Char('h') => {
+                        cursor.x = cursor.x.saturating_sub(1);
+                    }
+                    KeyCode::Char('j') => {
+                        cursor.y = cursor.y.saturating_add(1);
+                    }
+                    KeyCode::Char('k') => {
+                        cursor.y = cursor.y.saturating_sub(1);
+                    }
+                    KeyCode::Char('l') => {
+                        cursor.x = cursor.x.saturating_add(1);
+                    }
                     KeyCode::Char('+') => {
                         app.counter = app.counter.saturating_add(1);
-                        app.rows.push(Row::new(vec!["7/9 1/2 1"]));
-                        //app.constrains.push(Constraint::Percentage(30));
+                        app.rows[0].push("7/9 1/2 1".into());
+                        app.constrains.push(Constraint::Max(10));
                     }
                     KeyCode::Char('-') => {
                         app.counter = app.counter.saturating_sub(1);
-                        app.rows.pop();
-                        //app.constrains.pop();
+                        app.rows[0].pop();
+                        app.constrains.pop();
                     }
                     _ => (),
                 }
