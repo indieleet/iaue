d6cb173a65bcd04566f5b67991c3fc6b879a5ce3
indieleet <indieleet@gmail.com>
2024-08-20 17:31:00 -0400

    added modes

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ use ratatui::{
 use std::io::{stdout, Result};
 
 #[derive(Debug, Default)]
-pub struct App {
+pub struct App<'a> {
     counter: u16,
     cursor: u8,
     items: Vec<Constraint>,
@@ -21,12 +21,33 @@ pub struct App {
     columns: u8,
     x_bound: u16,
     y_bound: u16,
-    rows: Vec<Vec<String>>,
+    rows: Vec<Vec<Cell<'a>>>,
     constrains: Vec<Constraint>,
 }
 
+
+#[derive(Debug)]
+enum Mode {
+    Normal,
+    Insert,
+    Visual,
+}
+
+
 #[derive(Debug, Default)]
-struct AppCursor {
+struct NormalCursor {
+    x: u16,
+    y: u16,
+}
+
+#[derive(Debug, Default)]
+struct InsertCursor {
+    x: u16,
+    y: u16,
+}
+
+#[derive(Debug, Default)]
+struct VisualCursor {
     x: u16,
     y: u16,
 }
@@ -36,7 +57,10 @@ fn main() -> Result<()> {
     enable_raw_mode()?;
     let mut terminal = Terminal::new(CrosstermBackend::new(stdout()))?;
     terminal.clear()?;
-    let mut cursor = AppCursor::default();
+    let mut normal_cursor = NormalCursor::default();
+    let mut insert_cursor = InsertCursor::default();
+    let mut visual_cursor = VisualCursor::default();
+    let mut current_mode = Mode::Normal;
     let mut app = App {
         counter: 1,
         cursor: 0,
@@ -46,12 +70,48 @@ fn main() -> Result<()> {
         current_times: "".into(),
         items: vec![Constraint::Percentage(30), Constraint::Fill(1)],
         rows: vec![
-            vec!["7/9 1/2 1".into(), "1/2 3/5 1/2".into()],
-            vec!["3/2 1 1".into()],
+            vec![Cell::from("7/9 1/2 1"), Cell::from("1/2 3/5 1/2")],
+            vec![Cell::from("3/2 1 1")],
         ],
-        constrains: vec![Constraint::Max(10)],
+        constrains: vec![Constraint::Max(10), Constraint::Max(10)],
     };
+    //let mut state = TableState::new();
     loop {
+        let mut table_rows = app.rows.to_owned();
+        let mut cur_cell_text = Cell::default();
+        for (i_row, c_row) in table_rows.iter_mut().enumerate() {
+            for (i_el, el) in c_row.iter_mut().enumerate() {
+                match current_mode {
+                    Mode::Normal if (normal_cursor.x as usize, normal_cursor.y as usize) == (i_el, i_row) => {
+                        cur_cell_text = el.clone().add_modifier(Modifier::REVERSED);
+                    },
+                    Mode::Visual if (normal_cursor.x as usize, normal_cursor.y as usize) == (i_el, i_row) => {
+                        cur_cell_text = el.clone().add_modifier(Modifier::REVERSED);
+                    },
+                    Mode::Insert if (normal_cursor.x as usize, normal_cursor.y as usize) == (i_el, i_row) => {
+                        cur_cell_text = el.clone().add_modifier(Modifier::REVERSED);
+                    },
+                    _ => ()
+                }
+            }
+        }
+        //let y_bound = core::iter::repeat_with(|| &app.rows.iter().next().unwrap_or(&Vec::<Cell>::new()).get(normal_cursor.x as usize)).count();
+        let mut y_bound: u16 = 0;
+        for el in &app.rows {
+            if el.get(normal_cursor.x as usize).is_some() {
+                y_bound += 1;
+            }
+            else {
+                break;
+            }
+        }
+        table_rows[normal_cursor.y as usize][normal_cursor.x as usize] = cur_cell_text;
+        let mut mode_str = match current_mode { 
+            Mode::Normal => "Normal",
+            Mode::Visual => "Visual",
+            Mode::Insert => "Insert",
+            _ => todo!()
+            };
         terminal.draw(|f| {
             app.x_bound = f.size().width;
             app.y_bound = f.size().height;
@@ -61,15 +121,28 @@ fn main() -> Result<()> {
                 .split(f.size());
             f.render_widget(
                 Table::new(
-                    app.rows.to_owned().into_iter().map(Row::new),
+                    table_rows.into_iter().map(Row::new),
                     app.constrains.to_owned(),
                 )
                 .block(Block::bordered()),
                 size_x[0],
             );
+
+            //for (i_row, c_row) in app.rows.iter().enumerate() { 
+            //    for (i_el, c_el) in c_row {
+            //        f.render_widget(c_el, app.constrains[i_row][i_el])
+            //    }
+            //}
             f.render_widget(
                 Paragraph::new(format!("Hello {}", app.counter)).block(Block::bordered()),
                 size_x[1],
+            );
+            f.render_widget(mode_str, layout::Rect {
+                x: 0,
+                y: app.y_bound - 1,
+                width: mode_str.len() as u16,
+                height: 1
+            }
             );
             let ct = &app.current_times;
             f.render_widget(
@@ -82,15 +155,15 @@ fn main() -> Result<()> {
                 },
 
             );
-            f.render_widget(
-                " ".on_red(),
-                layout::Rect {
-                    x: cursor.x,
-                    y: cursor.y,
-                    width: 1,
-                    height: 1,
-                },
-            )
+            //f.render_widget(
+            //    " ".on_red(),
+            //    layout::Rect {
+            //        x: cursor.x,
+            //        y: cursor.y,
+            //        width: 1,
+            //        height: 1,
+            //    },
+            //)
         })?;
         // TODO main loop
         let match_event = event::read()?;
@@ -117,6 +190,7 @@ fn main() -> Result<()> {
                 ..
             }) => 
             {
+                current_mode = Mode::Normal;
                 let _ = &app.current_times.clear();
             },
             Event::Key(KeyEvent {
@@ -127,7 +201,14 @@ fn main() -> Result<()> {
             {
                 let count: u16 = app.current_times.parse().unwrap_or(1);
                 let _ = &app.current_times.clear();
-                cursor.x = cursor.x.saturating_sub(count);
+                match current_mode {
+                    Mode::Normal | Mode::Visual => { 
+                        //let x_bound = app.rows[normal_cursor.y as usize].len() as u16;
+                        normal_cursor.x = normal_cursor.x.saturating_sub(count);
+                    },
+                    Mode::Insert => todo!(),
+                }
+                //normal_cursor.x = normal_cursor.x.saturating_sub(count);
             },
             Event::Key(KeyEvent {
                 //modifiers: KeyModifiers::CONTROL,
@@ -138,9 +219,18 @@ fn main() -> Result<()> {
                 let count: u16 = app.current_times.parse().unwrap_or(1);
                 let _ = &app.current_times.clear();
                 //cursor.y = cursor.y.saturating_add(count);
-                let new_y = cursor.y.saturating_add(count);
-                cursor.y = if new_y > app.y_bound - 1 { app.y_bound - 1 }
-                    else { new_y };
+                let new_y = normal_cursor.y.saturating_add(count);
+                match current_mode {
+                    Mode::Normal | Mode::Visual => { 
+                        //let y_bound = app.rows[normal_cursor.y as usize].len() as u16;
+                        let new_y = normal_cursor.y.saturating_add(count);
+                        normal_cursor.y = if new_y > y_bound - 1 { y_bound - 1 }
+                            else { new_y };
+                    },
+                    Mode::Insert => todo!(),
+                }
+                //normal_cursor.y = if new_y > app.y_bound - 1 { app.y_bound - 1 }
+                //    else { new_y };
             },
             Event::Key(KeyEvent {
                 //modifiers: KeyModifiers::CONTROL,
@@ -150,7 +240,13 @@ fn main() -> Result<()> {
             {
                 let count: u16 = app.current_times.parse().unwrap_or(1);
                 let _ = &app.current_times.clear();
-                cursor.y = cursor.y.saturating_sub(count);
+                match current_mode {
+                    Mode::Normal | Mode::Visual => { 
+                        normal_cursor.y = normal_cursor.y.saturating_sub(count);
+                    },
+                    Mode::Insert => todo!(),
+                }
+                normal_cursor.y = normal_cursor.y.saturating_sub(count);
             },
             Event::Key(KeyEvent {
                 //modifiers: KeyModifiers::CONTROL,
@@ -160,9 +256,32 @@ fn main() -> Result<()> {
             {
                 let count: u16 = app.current_times.parse().unwrap_or(1);
                 let _ = &app.current_times.clear();
-                let new_x = cursor.x.saturating_add(count);
-                cursor.x = if new_x > app.x_bound - 1 { app.x_bound - 1 }
-                    else { new_x };
+                match current_mode {
+                    Mode::Normal | Mode::Visual => { 
+                        let x_bound = app.rows[normal_cursor.y as usize].len() as u16;
+                        let new_x = normal_cursor.x.saturating_add(count);
+                        normal_cursor.x = if new_x > x_bound - 1 { x_bound - 1 }
+                            else { new_x };
+                    },
+                    Mode::Insert => todo!(),
+                }
+                //cursor.x = if new_x > app.x_bound - 1 { app.x_bound - 1 }
+                //    else { new_x };
+            },
+            Event::Key(KeyEvent {
+                //modifiers: KeyModifiers::CONTROL,
+                code: KeyCode::Char('+'),
+                ..
+            }) => 
+            {},
+            Event::Key(KeyEvent {
+                //modifiers: KeyModifiers::CONTROL,
+                code: KeyCode::Char('v'),
+                ..
+            }) => 
+            {
+                current_mode = Mode::Visual;
+                (visual_cursor.x, visual_cursor.y) = (normal_cursor.x, normal_cursor.y);
             },
                    // KeyCode::Char('h') => {
                    //     cursor.x = cursor.x.saturating_sub(1);
@@ -186,7 +305,7 @@ fn main() -> Result<()> {
                    //     app.rows[0].pop();
                    //     app.constrains.pop();
                    // }
-                    _ => (),
+            _ => (),
                 }
     }
         
